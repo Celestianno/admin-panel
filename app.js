7cdba8bc98860c2f4f7eec417b3fc5c8f4ccc8a9
Celestianno <crazysingularity@gmail.com>
2024-04-29 16:17:47 +0300

    add project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mainRoute = require("./routes/main");
+const gamesRouter = require("./routes/games");
+const { cors } = require("./middlewares");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors, bodyParser.json(), express.static(path.join(__dirname, "public")), mainRoute, gamesRouter);
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+})
